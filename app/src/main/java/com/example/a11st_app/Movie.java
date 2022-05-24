99c25742a470af994bab003c4efbc1f3197778a2
sujin1030 <wjdtnwls1247@naver.com>
2022-05-24 16:15:56 +0900

    영화정보의수 추가

---
+++ app/src/main/java/com/example/a11st_app/Movie.java
@@ -0,0 +1,22 @@
+package com.example.a11st_app;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rankOldAndNew;
+    String movieCd;
+    String movieNm;
+    String openDt;
+    String saleAmt;
+    String saleShare;
+    String salesChange;
+    String saleAcc;
+    String audCnt;
+    String audiInten;
+    String audiChange;
+    String audiAcc;
+    String scrnCnt;
+    String showCnt;
+
+}
